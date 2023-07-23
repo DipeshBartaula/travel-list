fd68115afc3e879c2ed3d7675cda2d5f705fc2c1
DipesBartaula <dipeshbartaula4@gmail.com>
2023-07-23 23:48:42 +0545

    Add form and render list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import "./index.js";
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 9, packed: false },
+];
 export default function App() {
   return (
     <div className="app">
@@ -15,15 +19,35 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 🤩 trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item ..." />
+      <button>Add</button>
+    </form>
   );
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 function Stats() {
@@ -31,5 +55,16 @@ function Stats() {
     <footer className="stats">
       <em>👜 You have X item on your list, and you already pack X(X%)</em>
     </footer>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
   );
 }
